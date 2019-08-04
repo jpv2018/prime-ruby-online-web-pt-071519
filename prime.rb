5859897f97a599ae2329406289202e3ffa710650
Jason Viola <jason.viola@mavs.uta.edu>
2019-08-04 18:11:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(number)
+  if number < 0 
+    number * -1
+  end
+  numbers = (2...number)
+  results = [ ]
+  numbers.each do |n|
+    results << number % n 
+  end
+  if results.any? {|x| x == 0}
+    return false
+  else 
+    return true
+  end
+end
