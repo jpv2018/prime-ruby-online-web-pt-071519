bbed2f1d31c28e7dbd48befe62f6c2845426bda5
Jason Viola <jason.viola@mavs.uta.edu>
2019-08-04 18:17:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,26 +1,12 @@
 def prime?(number)
-  if number < 0 
-    number * -1
-    numbers = (2...number)
-    results = [ ]
-    numbers.each do |n|
-      results << number % n 
-    end
-    if results.any? {|x| x == 0}
-      return false
-    else 
-      return true
-    end
-  else 
-    numbers = (2...number)
-    results = [ ]
-    numbers.each do |n|
-      results << number % n 
-    end
+  numbers = (2...number)
+  results = [ ]
+  numbers.each do |n|
+    results << number % n 
+  end
   if results.any? {|x| x == 0}
     return false
   else 
     return true
-    end
   end
 end
